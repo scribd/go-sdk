2107045105c3d90ec5d3d306ad04179ed94c7155
Ilija Eftimov <ilija@scribd.com>
2019-10-15 15:59:40 +0200

    Add app package

---
+++ pkg/app/config.go
@@ -0,0 +1,96 @@
+package app
+
+import (
+	"os"
+	"path"
+	"time"
+
+	"github.com/spf13/viper"
+)
+
+// Config is custom application configuration.
+type Config struct {
+	vConf *viper.Viper
+}
+
+// NewDefaultConfig returns a new Config with default values
+func NewDefaultConfig() (*Config, error) {
+	return NewConfig(
+		path.Join(os.Getenv("APP_ROOT"), "config"),
+		"settings",
+	)
+}
+
+// NewConfig sets up the app configuration, setting default values and configurations.
+func NewConfig(configPath string, configName string) (*Config, error) {
+	conf := &Config{
+		vConf: viper.New(),
+	}
+
+	conf.vConf.AddConfigPath(configPath)
+	conf.vConf.SetConfigName(configName)
+	conf.vConf.SetConfigType("yaml")
+	conf.vConf.SetEnvPrefix("APP")
+	conf.vConf.AutomaticEnv()
+
+	if err := conf.vConf.ReadInConfig(); err != nil {
+		return conf, err
+	}
+
+	return conf, nil
+}
+
+// GetBool returns a key's value as bool.
+func (c *Config) GetBool(key string) bool {
+	return c.vConf.GetBool(key)
+}
+
+// GetFloat64 returns a key's value as float64.
+func (c *Config) GetFloat64(key string) float64 {
+	return c.vConf.GetFloat64(key)
+}
+
+// GetInt returns a key's value as int.
+func (c *Config) GetInt(key string) int {
+	return c.vConf.GetInt(key)
+}
+
+// GetString returns a key's value as string.
+func (c *Config) GetString(key string) string {
+	return c.vConf.GetString(key)
+}
+
+// GetStringMap returns a key's value as map[string]interface{}.
+func (c *Config) GetStringMap(key string) map[string]interface{} {
+	return c.vConf.GetStringMap(key)
+}
+
+// GetStringMapString returns a key's value as map[string]string.
+func (c *Config) GetStringMapString(key string) map[string]string {
+	return c.vConf.GetStringMapString(key)
+}
+
+// GetStringSlice returns a key's value as []string.
+func (c *Config) GetStringSlice(key string) []string {
+	return c.vConf.GetStringSlice(key)
+}
+
+// GetTime returns a key's value as time.Time.
+func (c *Config) GetTime(key string) time.Time {
+	return c.vConf.GetTime(key)
+}
+
+// GetDuration returns a key's value as time.Duration.
+func (c *Config) GetDuration(key string) time.Duration {
+	return c.vConf.GetDuration(key)
+}
+
+// IsSet checks if the key has assigned value.
+func (c *Config) IsSet(key string) bool {
+	return c.vConf.IsSet(key)
+}
+
+// AllSettings returns all settings as map.
+func (c *Config) AllSettings() map[string]interface{} {
+	return c.vConf.AllSettings()
+}
---
+++ pkg/app/config_test.go
@@ -0,0 +1,39 @@
+package app
+
+import "testing"
+
+func TestNewConfig(t *testing.T) {
+	cases := []struct {
+		name       string
+		configName string
+		want       bool
+	}{
+		{
+			name:       "ValidFile",
+			configName: "valid",
+			want:       false,
+		},
+		{
+			name:       "InvalidFile",
+			configName: "invalid",
+			want:       true,
+		},
+		{
+			name:       "InvalidFile",
+			configName: "nonexsistent",
+			want:       true,
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			_, err := NewConfig("testdata", c.configName)
+
+			got := err != nil
+
+			if c.want != got {
+				t.Errorf("Expected to get %v, got %v", c.want, got)
+			}
+		})
+	}
+}
