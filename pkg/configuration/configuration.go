d5d09ca6d5ad42d53a0242e2fe19e6f0ecd0c33b
Edoardo Rossi <edoardor@scribd.com>
2019-10-18 10:55:55 +0200

    Expose the LoggerConfig through the Config struct

--- pkg/configuration/configuration.go
+++ pkg/configuration/configuration.go
@@ -2,6 +2,7 @@ package configuration
 
 import (
 	app "git.lo/microservices/sdk/go-sdk/pkg/app"
+	logger "git.lo/microservices/sdk/go-sdk/pkg/logger"
 	server "git.lo/microservices/sdk/go-sdk/pkg/server"
 )
 
@@ -9,6 +10,7 @@ import (
 type Config struct {
 	App    *app.Config
 	Server *server.Config
+	Logger *logger.Config
 }
 
 // NewConfig returns a new Config instance
@@ -20,12 +22,18 @@ func NewConfig() (*Config, error) {
 		return config, err
 	}
 
+	loggerConfig, err := logger.NewConfig()
+	if err != nil {
+		return config, err
+	}
+
 	serverConfig, err := server.NewConfig()
 	if err != nil {
 		return config, err
 	}
 
 	config.App = appConfig
+	config.Logger = loggerConfig
 	config.Server = serverConfig
 
 	return config, nil
