2a63a868bfa677eba48869f00b48a7ee2548da92
Ilija Eftimov <ilija@scribd.com>
2019-10-24 15:38:56 +0200

    Add Database package

---
+++ pkg/database/config.go
@@ -0,0 +1,35 @@
+package database
+
+import (
+	"fmt"
+
+	cbuilder "git.lo/microservices/sdk/go-sdk/internal/pkg/configuration/builder"
+)
+
+// Config is the database connection configuration.
+type Config struct {
+	Host     string `mapstructure:"host"`
+	Port     int    `mapstructure:"port"`
+	Username string `mapstructure:"username"`
+	Password string `mapstructure:"password"`
+	Database string `mapstructure:"database"`
+	Pool     int    `mapstructure:"pool"`
+	Timeout  string `mapstructure:"timeout"`
+}
+
+// NewConfig returns a new Config instance.
+func NewConfig() (*Config, error) {
+	config := &Config{}
+	viperBuilder := cbuilder.New().ConfigName("database")
+
+	vConf, err := viperBuilder.Build()
+	if err != nil {
+		return config, err
+	}
+
+	if err = vConf.Unmarshal(config); err != nil {
+		return config, fmt.Errorf("Unable to decode into struct: %s", err.Error())
+	}
+
+	return config, nil
+}
