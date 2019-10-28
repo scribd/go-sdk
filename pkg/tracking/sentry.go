911a489aa2811782af72092f183c53d4f20f456f
Edoardo Rossi <edoardor@scribd.com>
2019-10-28 16:22:54 +0100

    Enable the StackTrace reporting on Sentry
    The default StackTraceConfiguration is
    
    StacktraceConfiguration: StackTraceConfiguration{
    	Enable:            false,
    	Level:             logrus.ErrorLevel,
    	Skip:              6,
    	Context:           0,
    	InAppPrefixes:     nil,
    	SendExceptionType: true,
    },
    
    - StacktraceConfiguration.Level the logrus level at which to start
      capturing stacktraces.
    - StacktraceConfiguration.Skip how many stack frames to skip before
      stacktrace starts recording.
    - StacktraceConfiguration.Context the number of lines to include around
      a stack frame for context.
    - StacktraceConfiguration.InAppPrefixes the prefixes that will be
      matched against the stack frame to identify it as in_app
    - StacktraceConfiguration.IncludeErrorBreadcrumb whether to create a
      breadcrumb with the full text of error

--- pkg/tracking/sentry.go
+++ pkg/tracking/sentry.go
@@ -9,11 +9,19 @@ import (
 // and initializes the raven client.
 // This method sets the timeout to 100 milliseconds.
 func NewSentryHook(config *Config) (*logrus_sentry.SentryHook, error) {
-	return logrus_sentry.NewSentryHook(
+	hook, err := logrus_sentry.NewSentryHook(
 		config.SentryDSN,
 		[]logrus.Level{
 			logrus.PanicLevel,
 			logrus.FatalLevel,
 			logrus.ErrorLevel,
 		})
+
+	if err != nil {
+		return hook, err
+	}
+
+	hook.StacktraceConfiguration.Enable = true
+
+	return hook, err
 }
