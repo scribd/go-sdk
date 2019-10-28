aca98eb7460db9500926eb1b8c16c4cc674ae5d8
Edoardo Rossi <edoardor@scribd.com>
2019-10-28 14:31:40 +0100

    Introduce the Logger Builder
    the Builder is the only way to create, configure, instrument and return
    a Logger implementation.
    
    The creation of a logger can be summarised as:
    
    ```
    fields := map[string]string{ "role": "server" }
    logger, err := NewBuilder(config).SetFields(fields).SetTracking(trackingConfig).Build()
    ```

---
+++ pkg/logger/builder.go
@@ -0,0 +1,67 @@
+package logger
+
+import (
+	"bytes"
+
+	"git.lo/microservices/sdk/go-sdk/pkg/tracking"
+)
+
+// Builder is a Logger builder.
+type Builder struct {
+	config         *Config
+	fields         Fields
+	trackingConfig *tracking.Config
+}
+
+// NewBuilder initialises a Logger builder with the given configuration.
+func NewBuilder(config *Config) *Builder {
+	return &Builder{
+		config: config,
+	}
+}
+
+// SetFields adds Fields to the Builder.
+func (b *Builder) SetFields(fields Fields) *Builder {
+	b.fields = fields
+	return b
+}
+
+// SetTracking sets the error reporting configuration.
+func (b *Builder) SetTracking(trackingConfig *tracking.Config) *Builder {
+	b.trackingConfig = trackingConfig
+	return b
+}
+
+// Build applies the given configuration and returns a Logger instance.
+func (b *Builder) Build() (Logger, error) {
+	lLogrus, err := newLogrusLogger(b.config)
+	if err != nil {
+		return nil, err
+	}
+
+	logrusEntry := logrusLogEntry{
+		entry: lLogrus.WithFields(convertToLogrusFields(b.fields)),
+	}
+
+	if b.trackingConfig != nil {
+		if err := logrusEntry.setTracking(b.trackingConfig); err != nil {
+			return nil, err
+		}
+	}
+
+	return &logrusEntry, nil
+}
+
+// BuildTestLogger returns a Logger instance that will write into the bytes buffer
+// passed as parameter.
+// BuildTestLogger is only for testing.
+func (b *Builder) BuildTestLogger(out *bytes.Buffer) (Logger, error) {
+	lLogrus, err := newTestLogrusLogger(b.config, out)
+	if err != nil {
+		return nil, err
+	}
+
+	return &logrusLogEntry{
+		entry: lLogrus.WithFields(convertToLogrusFields(b.fields)),
+	}, nil
+}
