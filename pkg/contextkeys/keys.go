06dd8ab38883fc06cdb95a3f27765972a3588751
Ilija Eftimov <ilija@scribd.com>
2019-11-06 18:13:30 +0100

    Add Database to contextkeys

--- pkg/contextkeys/keys.go
+++ pkg/contextkeys/keys.go
@@ -6,4 +6,7 @@ const (
 
 	// RequestID is the context key for the carrying the RequestID.
 	RequestID = "RequestID"
+
+	// Database is the context key for the carrying the Database connection pool.
+	Database = "Database"
 )
