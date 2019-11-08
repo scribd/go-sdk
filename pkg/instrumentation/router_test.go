ba5f96144de2766efa706dac44baea6d39da6bc5
Edoardo Rossi <edoardor@scribd.com>
2019-11-08 09:38:48 +0100

    Add instrumentation.Router tests

---
+++ pkg/instrumentation/router_test.go
@@ -0,0 +1,36 @@
+package instrumentation
+
+import (
+	"testing"
+
+	assert "github.com/stretchr/testify/assert"
+)
+
+func TestNewTracer(t *testing.T) {
+	testCases := []struct {
+		name      string
+		enabled   bool
+		wantError bool
+	}{
+		{
+			name:      "WithAConfigurationIsAlwaysOkWhenEnabled",
+			enabled:   true,
+			wantError: false,
+		},
+		{
+			name:      "WithAConfigurationIsAlwaysOkWhenDisabled",
+			enabled:   false,
+			wantError: false,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			config := &Config{
+				Enabled: tc.enabled,
+			}
+			trcr := NewTracer(config)
+			assert.Equal(t, trcr.Enabled, tc.enabled)
+		})
+	}
+}
