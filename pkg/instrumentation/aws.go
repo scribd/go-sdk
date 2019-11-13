c105aa72cd167d08f4d403547e06d43017f7d63a
Edoardo Rossi <edoardor@scribd.com>
2019-11-13 14:13:25 +0100

    Attach the service name to the AWS service traced
    `WithServiceName` sets the given service name for the dialled connection.
    When the service name is not explicitly set it will be inferred based on
    the request to AWS.
    
    We automagically impose the convention of appending the `-aws` suffix.
    
    Ref:
    https://godoc.org/gopkg.in/DataDog/dd-trace-go.v1/contrib/aws/aws-sdk-go/aws

--- pkg/instrumentation/aws.go
+++ pkg/instrumentation/aws.go
@@ -7,8 +7,9 @@ import (
 	awstrace "gopkg.in/DataDog/dd-trace-go.v1/contrib/aws/aws-sdk-go/aws"
 )
 
-func InstrumentAWSSession(s *awssession.Session) *awssession.Session {
+func InstrumentAWSSession(s *awssession.Session, appName string) *awssession.Session {
 	return awstrace.WrapSession(
 		s,
+		awstrace.WithServiceName(fmt.Sprintf("%s-%s", appName, "aws")),
 	)
 }
