281d91c1f16bfa3a8bc3eabee610710003f0b06b
Edoardo Rossi <edoardor@scribd.com>
2020-08-07 13:45:17 +0200

    feat: Move the `Metrics` Context key in the package `contextkeys`
    This change is aiming to maintain the consistency with the current
    approach.

--- pkg/contextkeys/keys.go
+++ pkg/contextkeys/keys.go
@@ -9,4 +9,7 @@ const (
 
 	// Database is the context key for the carrying the Database connection pool.
 	Database = "Database"
+
+	// Metrics is the context key for the carrying the Metrics client.
+	Metrics = "Metrics"
 )
