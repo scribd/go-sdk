fd03e2bff9f9d974fa1c14fda76fdb6777a19eb0
Edoardo Rossi <edoardor@scribd.com>
2020-09-09 12:05:15 +0200

    feat: Inject the Metrics in the context using the context/metrics package

---
+++ pkg/context/metrics/context.go
@@ -0,0 +1,38 @@
+package metrics
+
+import (
+	"context"
+	"fmt"
+
+	sdkmetrics "git.lo/microservices/sdk/go-sdk/pkg/metrics"
+)
+
+type ctxMetricsMarker struct{}
+
+type ctxMetrics struct {
+	metrics sdkmetrics.Metrics
+}
+
+var (
+	ctxMetricsKey = &ctxMetricsMarker{}
+)
+
+// Extract takes the call-scoped sdkmetrics.Metrics from the context.
+// If the ctxMetrics  wasn't used, an error is returned.
+func Extract(ctx context.Context) (sdkmetrics.Metrics, error) {
+	m, ok := ctx.Value(ctxMetricsKey).(*ctxMetrics)
+	if !ok || m == nil {
+		return nil, fmt.Errorf("Unable to get the metrics")
+	}
+
+	return m.metrics, nil
+}
+
+// ToContext adds the sdkmetrics.Metrics to the context for extraction later.
+// Returning the new context that has been created.
+func ToContext(ctx context.Context, metrics sdkmetrics.Metrics) context.Context {
+	m := &ctxMetrics{
+		metrics: metrics,
+	}
+	return context.WithValue(ctx, ctxMetricsKey, m)
+}
