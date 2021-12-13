8fe5b26e6344e21f8607c83eb21428a939ce8d2f
Maksim Terekhin <maksimt@scribd.com>
2021-12-13 13:34:53 +0100

    feat: Move gorm logger to logger package

---
+++ pkg/logger/gorm_test.go
@@ -0,0 +1,122 @@
+package logger
+
+import (
+	"bytes"
+	"encoding/json"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"testing"
+	"time"
+)
+
+func TestNewGormLogger(t *testing.T) {
+	tests := []struct {
+		name     string
+		input    []interface{}
+		cfg      Config
+		isEmpty  bool
+		expected map[string]interface{}
+	}{
+		{
+			name:    "Empty on fatal log level",
+			input:   []interface{}{"sql", "test/test", time.Second, "select * from test;", []interface{}{}, int64(10)},
+			isEmpty: true,
+			cfg: Config{
+				ConsoleEnabled:    true,
+				ConsoleJSONFormat: true,
+				ConsoleLevel:      "fatal",
+			},
+		},
+		{
+			name:    "Empty on error log level",
+			input:   []interface{}{"sql", "test/test", time.Second, "select * from test;", []interface{}{}, int64(10)},
+			isEmpty: true,
+			cfg: Config{
+				ConsoleEnabled:    true,
+				ConsoleJSONFormat: true,
+				ConsoleLevel:      "error",
+			},
+		},
+		{
+			name:    "Empty on warning log level",
+			input:   []interface{}{"sql", "test/test", time.Second, "select * from test;", []interface{}{}, int64(10)},
+			isEmpty: true,
+			cfg: Config{
+				ConsoleEnabled:    true,
+				ConsoleJSONFormat: true,
+				ConsoleLevel:      "warning",
+			},
+		},
+		{
+			name:    "Empty on info log level",
+			input:   []interface{}{"sql", "test/test", time.Second, "select * from test;", []interface{}{}, int64(10)},
+			isEmpty: true,
+			cfg: Config{
+				ConsoleEnabled:    true,
+				ConsoleJSONFormat: true,
+				ConsoleLevel:      "info",
+			},
+		},
+		{
+			name:  "Print database log",
+			input: []interface{}{"sql", "test/test", time.Second, "select * from test;", []interface{}{}, int64(10)},
+			cfg: Config{
+				ConsoleEnabled:    true,
+				ConsoleJSONFormat: true,
+				ConsoleLevel:      "debug",
+			},
+			expected: map[string]interface{}{
+				"duration":      float64(time.Second),
+				"affected_rows": float64(10),
+				"file_location": "test/test",
+				"values":        "",
+			},
+		},
+		{
+			name:  "Empty on non sql input in debug mode",
+			input: []interface{}{"test string", "test/test", time.Second, "select * from test;", []interface{}{}, int64(10)},
+			cfg: Config{
+				ConsoleEnabled:    true,
+				ConsoleJSONFormat: true,
+				ConsoleLevel:      "debug",
+			},
+			isEmpty: true,
+		},
+		{
+			name:  "Print log in trace mode on non sql input",
+			input: []interface{}{"test string", "test/test", time.Second, "select * from test;", []interface{}{}, int64(10)},
+			cfg: Config{
+				ConsoleEnabled:    true,
+				ConsoleJSONFormat: true,
+				ConsoleLevel:      "trace",
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var fields Fields
+			var buffer bytes.Buffer
+
+			b := NewBuilder(&tt.cfg)
+			l, err := b.BuildTestLogger(&buffer)
+			require.Nil(t, err)
+
+			gl := NewGormLogger(l)
+			gl.Print(tt.input...)
+
+			if tt.isEmpty {
+				assert.Empty(t, buffer.Bytes())
+			} else {
+				err = json.Unmarshal(buffer.Bytes(), &fields)
+				assert.Nil(t, err)
+
+				if tt.expected != nil {
+					assert.Equal(t, tt.expected, fields["sql"])
+				} else {
+					assert.NotEmpty(t, fields["message"])
+				}
+			}
+		})
+	}
+}
