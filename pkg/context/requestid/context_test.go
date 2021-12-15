7997f79ecc440bd88854522f35302fba3aac9347
Maksim Terekhin <maksimt@scribd.com>
2021-12-15 12:53:04 +0100

    feat: Add context helpers for request id

---
+++ pkg/context/requestid/context_test.go
@@ -0,0 +1,47 @@
+package requestid
+
+import (
+	"context"
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestExtract(t *testing.T) {
+	tests := []struct {
+		name          string
+		ctxSet        func(ctx context.Context) context.Context
+		expected      string
+		expectedError error
+	}{
+		{
+			name: "Context without request id",
+			ctxSet: func(ctx context.Context) context.Context {
+				return ctx
+			},
+			expectedError: fmt.Errorf("Unable to get the requestID"),
+		},
+		{
+			name: "Context contains request id",
+			ctxSet: func(ctx context.Context) context.Context {
+				return ToContext(ctx, "test")
+			},
+			expected: "test",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ctx := context.Background()
+			resultCtx := tt.ctxSet(ctx)
+
+			requestId, err := Extract(resultCtx)
+
+			if tt.expectedError != nil {
+				assert.Equal(t, tt.expectedError, err)
+			} else {
+				assert.Equal(t, tt.expected, requestId)
+			}
+		})
+	}
+}
