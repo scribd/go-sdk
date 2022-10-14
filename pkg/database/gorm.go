3a6a64da7c2b55ff1ebbbdb3e951f46ba070b594
Daniel Bedrood <danielb@scribd.com>
2022-10-14 13:34:35 +0200

    chore: Refactor pkg/database to adapt gorm v2

--- pkg/database/gorm.go
+++ pkg/database/gorm.go
@@ -1,31 +1,50 @@
 package database
 
 import (
-	"github.com/DATA-DOG/go-txdb"
-	"github.com/jinzhu/gorm"
+	"strconv"
+	"time"
 
-	// Imports required gorm MySQL dialect.
-	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/DATA-DOG/go-txdb"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
+
+const testEnv = "test"
 
 // NewConnection returns a new Gorm database connection.
 func NewConnection(config *Config, environment string) (*gorm.DB, error) {
-	var db *gorm.DB
-	var err error
-
 	connectionDetails := NewConnectionDetails(config)
 
-	switch environment {
-	case "test":
-		txdb.Register("txdb", connectionDetails.Dialect, connectionDetails.String())
-		db, err = gorm.Open(connectionDetails.Dialect, "txdb", "tx_1")
-	default:
-		db, err = gorm.Open(connectionDetails.Dialect, connectionDetails.String())
+	connectionString := connectionDetails.String()
+	driverName := connectionDetails.Dialect
+
+	// Register the test driver and mock driver name and connection string in test environment.
+	if environment == testEnv {
+		// Using time.Now() as a unique identifier for the test database so that we can call NewConnection()
+		// multiple times without getting an error.
+		testDriverName := strconv.Itoa(int(time.Now().UnixNano()))
+
+		txdb.Register(testDriverName, connectionDetails.Dialect, connectionString)
+		driverName = testDriverName
+		connectionString = testDriverName
 	}
 
-	if err == nil {
-		db.DB().SetMaxIdleConns(connectionDetails.Pool)
+	dialector := mysql.New(mysql.Config{
+		DSN:        connectionString,
+		DriverName: driverName,
+	})
+
+	db, err := gorm.Open(dialector)
+	if err != nil {
+		return nil, err
 	}
 
-	return db, err
+	sqlDB, err := db.DB()
+	if err != nil {
+		return nil, err
+	}
+
+	sqlDB.SetMaxIdleConns(config.Pool)
+
+	return db, nil
 }
