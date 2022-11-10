d02056dd155f24c829d04bd4dbf229acd7d50742
Daniel Bedrood <danielb@scribd.com>
2022-11-10 17:41:35 +0100

    feat: Modify interceptors service name

--- pkg/interceptors/tracing.go
+++ pkg/interceptors/tracing.go
@@ -7,35 +7,32 @@ import (
 	grpctrace "gopkg.in/DataDog/dd-trace-go.v1/contrib/google.golang.org/grpc"
 )
 
-const (
-	datadogServiceServerSuffix = "grpc"
-	datadogServiceClientSuffix = "grpc-client"
-)
+const datadogServiceSuffix = "app"
 
 // TracingUnaryServerInterceptor returns a unary server interceptor that will
 // trace requests to the given gRPC server.
 func TracingUnaryServerInterceptor(applicationName string) grpc.UnaryServerInterceptor {
-	serviceName := fmt.Sprintf("%s-%s", applicationName, datadogServiceServerSuffix)
+	serviceName := fmt.Sprintf("%s-%s", applicationName, datadogServiceSuffix)
 	return grpctrace.UnaryServerInterceptor(grpctrace.WithServiceName(serviceName))
 }
 
 // TracingStreamServerInterceptor returns a stream server interceptor that will
 // trace streaming requests to the given gRPC server.
 func TracingStreamServerInterceptor(applicationName string) grpc.StreamServerInterceptor {
-	serviceName := fmt.Sprintf("%s-%s", applicationName, datadogServiceServerSuffix)
+	serviceName := fmt.Sprintf("%s-%s", applicationName, datadogServiceSuffix)
 	return grpctrace.StreamServerInterceptor(grpctrace.WithServiceName(serviceName))
 }
 
 // TracingUnaryClientInterceptor returns a unary client interceptor that will
 // trace requests performed by gRPC client.
 func TracingUnaryClientInterceptor(applicationName string) grpc.UnaryClientInterceptor {
-	serviceName := fmt.Sprintf("%s-%s", applicationName, datadogServiceClientSuffix)
+	serviceName := fmt.Sprintf("%s-%s", applicationName, datadogServiceSuffix)
 	return grpctrace.UnaryClientInterceptor(grpctrace.WithServiceName(serviceName))
 }
 
 // TracingStreamClientInterceptor returns a stream server interceptor that will
 // trace streaming requests performed by gRPC client.
 func TracingStreamClientInterceptor(applicationName string) grpc.StreamClientInterceptor {
-	serviceName := fmt.Sprintf("%s-%s", applicationName, datadogServiceClientSuffix)
+	serviceName := fmt.Sprintf("%s-%s", applicationName, datadogServiceSuffix)
 	return grpctrace.StreamClientInterceptor(grpctrace.WithServiceName(serviceName))
 }
