47a2e39e62a5ddf1c4da5b5c1873137e73865885
Daniel Bedrood <danielb@scribd.com>
2024-06-11 15:54:03 +0200

    feat: Replacing custom database ddtrace with dd-trace-go library

--- pkg/database/gorm.go
+++ pkg/database/gorm.go
@@ -1,18 +1,36 @@
 package database
 
 import (
+	"fmt"
 	"strconv"
 	"time"
 
 	"github.com/DATA-DOG/go-txdb"
+	gormtrace "gopkg.in/DataDog/dd-trace-go.v1/contrib/gorm.io/gorm.v1"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 const testEnv = "test"
 
-// NewConnection returns a new Gorm database connection.
-func NewConnection(config *Config, environment string) (*gorm.DB, error) {
+// NewConnection returns a new instrumented Gorm database connection.
+func NewConnection(config *Config, environment, appName string) (*gorm.DB, error) {
+	serviceName := fmt.Sprintf("%s-mysql", appName)
+	dialector := getDialectorFromConfig(config, environment)
+
+	db, err := gormtrace.Open(dialector, nil, gormtrace.WithServiceName(serviceName))
+	if err != nil {
+		return nil, err
+	}
+
+	if err := databasePoolSettings(db, config); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func getDialectorFromConfig(config *Config, environment string) gorm.Dialector {
 	connectionDetails := NewConnectionDetails(config)
 
 	connectionString := connectionDetails.String()
@@ -29,25 +47,22 @@ func NewConnection(config *Config, environment string) (*gorm.DB, error) {
 		connectionString = testDriverName
 	}
 
-	dialector := mysql.New(mysql.Config{
+	return mysql.New(mysql.Config{
 		DSN:        connectionString,
 		DriverName: driverName,
 	})
+}
 
-	db, err := gorm.Open(dialector)
+func databasePoolSettings(gormDB *gorm.DB, config *Config) error {
+	db, err := gormDB.DB()
 	if err != nil {
-		return nil, err
+		return err
 	}
 
-	sqlDB, err := db.DB()
-	if err != nil {
-		return nil, err
-	}
+	db.SetMaxIdleConns(config.Pool)
+	db.SetMaxOpenConns(config.MaxOpenConnections)
+	db.SetConnMaxIdleTime(config.ConnectionMaxIdleTime)
+	db.SetConnMaxLifetime(config.ConnectionMaxLifetime)
 
-	sqlDB.SetMaxIdleConns(config.Pool)
-	sqlDB.SetMaxOpenConns(config.MaxOpenConnections)
-	sqlDB.SetConnMaxIdleTime(config.ConnectionMaxIdleTime)
-	sqlDB.SetConnMaxLifetime(config.ConnectionMaxLifetime)
-
-	return db, nil
+	return nil
 }
--- pkg/instrumentation/database_test.go
+++ pkg/instrumentation/database_test.go
@@ -5,146 +5,40 @@ import (
 	"path"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/mocktracer"
+	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/tracer"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
-type TestRecord struct {
-	ID   int
-	Name string
-}
-
-func TestInstrumentDatabase(t *testing.T) {
-	expectedSpans := []struct {
-		name string
-		tags map[string]interface{}
-	}{
-		{
-			name: "SELECT",
-			tags: map[string]interface{}{
-				"db.count":      int64(-1),
-				"db.err":        nil,
-				"db.table":      "",
-				"service.name":  "test_app_name-mysql",
-				"span.type":     "sql",
-				"db.query":      `SELECT COUNT(*) FROM SQLITE_MASTER WHERE TYPE='TABLE' AND NAME=?`,
-				"resource.name": `SELECT COUNT(*) FROM SQLITE_MASTER WHERE TYPE='TABLE' AND NAME=?`,
-			},
-		},
-		{
-			name: "INSERT",
-			tags: map[string]interface{}{
-				"db.count":      int64(1),
-				"db.err":        nil,
-				"db.table":      "test_records",
-				"service.name":  "test_app_name-mysql",
-				"span.type":     "sql",
-				"db.query":      "INSERT INTO `TEST_RECORDS` (`NAME`,`ID`) VALUES (?,?) RETURNING `ID`",
-				"resource.name": "INSERT INTO `TEST_RECORDS` (`NAME`,`ID`) VALUES (?,?) RETURNING `ID`",
-			},
-		},
-		{
-			name: "UPDATE",
-			tags: map[string]interface{}{
-				"db.count":      int64(1),
-				"db.err":        nil,
-				"db.table":      "test_records",
-				"service.name":  "test_app_name-mysql",
-				"span.type":     "sql",
-				"db.query":      "UPDATE `TEST_RECORDS` SET `ID`=?,`NAME`=? WHERE `ID` = ? RETURNING `ID`",
-				"resource.name": "UPDATE `TEST_RECORDS` SET `ID`=?,`NAME`=? WHERE `ID` = ? RETURNING `ID`",
-			},
-		},
-		{
-			name: "SELECT",
-			tags: map[string]interface{}{
-				"db.count":      int64(1),
-				"db.err":        nil,
-				"db.table":      "test_records",
-				"service.name":  "test_app_name-mysql",
-				"span.type":     "sql",
-				"db.query":      "SELECT * FROM `TEST_RECORDS` WHERE `ID` = ? ORDER BY `TEST_RECORDS`.`ID` LIMIT 1",
-				"resource.name": "SELECT * FROM `TEST_RECORDS` WHERE `ID` = ? ORDER BY `TEST_RECORDS`.`ID` LIMIT 1",
-			},
-		},
-		{
-			name: "DELETE",
-			tags: map[string]interface{}{
-				"db.count":      int64(1),
-				"db.err":        nil,
-				"db.table":      "test_records",
-				"service.name":  "test_app_name-mysql",
-				"span.type":     "sql",
-				"db.query":      "DELETE FROM `TEST_RECORDS` WHERE `ID` = ? AND `TEST_RECORDS`.`ID` = ? RETURNING `ID`",
-				"resource.name": "DELETE FROM `TEST_RECORDS` WHERE `ID` = ? AND `TEST_RECORDS`.`ID` = ? RETURNING `ID`",
-			},
-		},
-	}
-
-	mt := mocktracer.Start()
-	defer mt.Stop()
-
-	dbFile := path.Join(t.TempDir(), "test_db")
-	db, err := gorm.Open(sqlite.Open(dbFile))
-	if err != nil {
-		t.Fatalf("Failed to open DB: %s", err)
-	}
-
-	InstrumentDatabase(db, "test_app_name")
-	db = TraceDatabase(context.Background(), db)
-
-	var (
-		testRecord    = TestRecord{ID: 1, Name: "test_name"}
-		updatedRecord = TestRecord{ID: 1, Name: "new_test_name"}
-		readRecord    = &TestRecord{}
+func TestTraceDatabase(t *testing.T) {
+	const (
+		mockOperationName = "test"
+		mockDBName        = "test_db"
 	)
 
-	if err = db.AutoMigrate(TestRecord{}); err != nil {
-		t.Fatalf("Failed to migrate DB: %s", err)
-	}
-
-	err = db.Begin().Create(&testRecord).
-		Save(&updatedRecord).
-		First(&readRecord).
-		Delete(&testRecord).
-		Commit().Error
-	if err != nil {
-		t.Fatalf("Failed to commit changes on test record: %s", err)
-	}
-
-	spans := mt.FinishedSpans()
-	if len(spans) != 5 {
-		t.Fatalf("Unexpected number of spans: %d", len(spans))
-	}
-
-	for i := range spans {
-		actualName := spans[i].OperationName()
-		actualTags := spans[i].Tags()
-
-		expectedName := expectedSpans[i].name
-		expectedTags := expectedSpans[i].tags
-
-		if actualName != expectedName {
-			t.Errorf("Got span: %s, expected: %s", actualName, expectedName)
-		}
-
-		assert.Equal(t, expectedTags, actualTags, "database tags didn't match")
-	}
-}
-
-func TestTraceDatabase(t *testing.T) {
-	dbFile := path.Join(t.TempDir(), "test_db")
+	dbFile := path.Join(t.TempDir(), mockDBName)
 	db, err := gorm.Open(sqlite.Open(dbFile))
 	if err != nil {
 		t.Fatalf("Failed to open DB: %s", err)
 	}
 
-	InstrumentDatabase(db, "test_app_name")
-	db = TraceDatabase(context.Background(), db)
+	mockTracer := mocktracer.Start()
+	defer mockTracer.Stop()
 
-	if sp := db.Statement.Context.Value(parentSpanGormKey); sp == nil {
-		t.Error("Parent span not set on statement")
+	mockSpan := tracer.StartSpan(mockOperationName)
+	defer mockSpan.Finish()
+
+	mockContext := tracer.ContextWithSpan(context.Background(), mockSpan)
+	db = TraceDatabase(mockContext, db)
+
+	_, ok := tracer.SpanFromContext(db.Statement.Context)
+	if !ok {
+		require.True(t, ok)
 	}
+
+	openSpans := mockTracer.OpenSpans()
+	require.Len(t, openSpans, 1)
+	require.Equal(t, openSpans[0].OperationName(), mockOperationName)
 }
