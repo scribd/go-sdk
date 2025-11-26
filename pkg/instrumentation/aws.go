fbee091fc97c3c305e4d13aef46c34b5b98e7560
Maksim Terekhin <maksimt@scribd.com>
2025-11-26 11:13:09 +0100

    feat: Remove aws-sdk-go v1

--- pkg/instrumentation/aws.go
+++ pkg/instrumentation/aws.go
@@ -4,10 +4,8 @@ import (
 	"fmt"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
-	awssession "github.com/aws/aws-sdk-go/aws/session"
 
 	awstracev2 "gopkg.in/DataDog/dd-trace-go.v1/contrib/aws/aws-sdk-go-v2/aws"
-	awstrace "gopkg.in/DataDog/dd-trace-go.v1/contrib/aws/aws-sdk-go/aws"
 )
 
 // Settings stores DataDog instrumentation settings.
@@ -15,18 +13,6 @@ type Settings struct {
 	AppName       string
 	Analytics     bool
 	AnalyticsRate float64
-}
-
-// InstrumentAWSSession configures DD tracing mode.
-//
-// Deprecated: Use InstrumentAWSClient instead.
-func InstrumentAWSSession(session *awssession.Session, settings Settings) *awssession.Session {
-	return awstrace.WrapSession(
-		session,
-		awstrace.WithServiceName(fmt.Sprintf("%s-aws", settings.AppName)),
-		awstrace.WithAnalytics(settings.Analytics),
-		awstrace.WithAnalyticsRate(settings.AnalyticsRate),
-	)
 }
 
 // InstrumentAWSClient configures DD tracing mode.
